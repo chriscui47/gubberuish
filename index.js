f763987f3552e7067388e29753a86d34f48da576
Christopher Cui <christopher.cui@mail.mcgill.ca>
2020-05-21 22:53:10 -0400

    fasd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { blueDeck, yellowDeck,redDeck,mixDeck} = require('./wordlist');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
