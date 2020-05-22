ebea5c6b2a308aac9ba4e242032709635666c4cc
Christopher Cui <christopher.cui@mail.mcgill.ca>
2020-05-22 17:52:38 -0400

    SDf

--- index.js
+++ index.js
@@ -34,18 +34,8 @@ const {users} = require('./users');
 const server = http.createServer(app);
 //const io = socketio(server);
 
+const io = socketio(server);
 
-const io = require("socket.io")(server, {
-  handlePreflightRequest: (req, res) => {
-      const headers = {
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-          "Access-Control-Allow-Credentials": true
-      };
-      res.writeHead(200, headers);
-      res.end();
-  }
-});
 
 
 
