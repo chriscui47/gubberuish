a0565591ab0befdf4af338f2175d795c9b73250f
Christopher Cui <christopher.cui@mail.mcgill.ca>
2020-05-22 13:41:36 -0400

    SDf

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ var bodyParser = require("body-parser");
 const { addUser, getUser, addRoom,rooms} = require('./users');
 const { blueDeck, yellowDeck,redDeck,mixDeck} = require('./wordlist');
 const app = express();
-app.use(cors());
 
-app.options('*', cors());
-/*
+
 app.use(function(req, res, next) {
   var allowedOrigins = ['http://localhost:5000', 'http://localhost:3000', 'https://playgibberish.com/', 'https://gibb47.herokuapp.com/','https://playgibberish.com/lobby'];
   var origin = req.headers.origin;
@@ -26,7 +24,6 @@ app.use(function(req, res, next) {
 
   return next();
 });
-*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
